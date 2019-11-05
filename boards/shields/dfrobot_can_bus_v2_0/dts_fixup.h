33dd91dabb61c5fe68f302f4931ea0e6b400bdd1
Karsten Koenig <karsten.koenig.030@gmail.com>
2019-11-05 21:02:30 +0100

    boards: shields: mcp2515-shields: Add dts_fixup.h
    If a MCP2515 shield is present we will declare this as the CAN_1
    instance so it could be referenced by the more common DT_CAN_1_NAME.
    
    Signed-off-by: Karsten Koenig <karsten.koenig.030@gmail.com>

---
+++ boards/shields/dfrobot_can_bus_v2_0/dts_fixup.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2019 Karsten Koenig
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* Shield level DTS fixup file */
+
+#define DT_CAN_1_NAME	DT_INST_0_MICROCHIP_MCP2515_LABEL
+
+/* End of Shield Level DTS fixup file */
