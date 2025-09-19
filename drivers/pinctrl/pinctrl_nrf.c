karstenkoenig/sdk-zephyr

drivers: pinctrl_nrf: Add coresight tpiu pins
Pinctrl needs to set the needed drive and direction of the pins. Also
this later allows automatically setting the clock bit for the traceclk
pin.

Signed-off-by: Karsten Koenig <karsten.koenig@nordicsemi.no>

drivers/pinctrl/pinctrl_nrf.c
@@ -582,6 +582,17 @@ int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt,
 			input = NRF_GPIO_PIN_INPUT_CONNECT;
 			break;
 #endif /* defined(NRF_PSEL_SDP_MSPI) */
+#if DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_coresight)
+		/* Pin routing is controlled by secure domain, via UICR */
+		case NRF_FUN_TPIU_CLOCK:
+		case NRF_FUN_TPIU_DATA0:
+		case NRF_FUN_TPIU_DATA1:
+		case NRF_FUN_TPIU_DATA2:
+		case NRF_FUN_TPIU_DATA3:
+			dir = NRF_GPIO_PIN_DIR_OUTPUT;
+			input = NRF_GPIO_PIN_INPUT_DISCONNECT;
+			break;
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_coresight) */
 		default:
 			return -ENOTSUP;
 		}

karstenkoenig/zephyr

drivers/pinctrl/pinctrl_nrf.c
@@ -549,6 +549,17 @@ int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt,
 			input = NRF_GPIO_PIN_INPUT_CONNECT;
 			break;
 #endif /* defined(NRF_PSEL_TWIS) */
+#if DT_HAS_COMPAT_STATUS_OKAY(nordic_coresight_nrf)
+		/* Pin routing is controlled by secure domain, via UICR */
+		case NRF_FUN_TPIU_CLOCK:
+		case NRF_FUN_TPIU_DATA0:
+		case NRF_FUN_TPIU_DATA1:
+		case NRF_FUN_TPIU_DATA2:
+		case NRF_FUN_TPIU_DATA3:
+			dir = NRF_GPIO_PIN_DIR_OUTPUT;
+			input = NRF_GPIO_PIN_INPUT_DISCONNECT;
+			break;
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(nordic_coresight_nrf) */
 		default:
 			return -ENOTSUP;
 		}